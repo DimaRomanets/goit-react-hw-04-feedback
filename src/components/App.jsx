d42390ca0e873f59389990b57158ffcb6ab67a12
DimaRomanets <130175375+DimaRomanets@users.noreply.github.com>
2023-11-09 22:29:05 +0100

    ok

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -6,38 +6,38 @@ import Notification from 'components/Notification/Notification';
 import 'modern-normalize/modern-normalize.css';
 
 const App = () => {
-    const [feedback, setFeedback] = useState({ good: 0, neutral: 0, bad: 0 });
+  const [feedback, setFeedback] = useState({ good: 0, neutral: 0, bad: 0 });
 
-    const countFeedback = text => {
-        setFeedback(state => ({ ...state, [text]: state[text] + 1 }));
-    };
+  const countFeedback = text => {
+    setFeedback(state => ({ ...state, [text]: state[text] + 1 }));
+  };
 
-    const { good, neutral, bad } = feedback;
-    const total = good + neutral + bad;
-    const percent = Math.floor((good / total) * 100);
-    return (
-        <div style={{ height: '100vh', fontSize: 32, color: '#010101' }}>
-            <Section title={'Please leave feedback'}>
-                <FeedbackOptions
-                    options={Object.keys(feedback)}
-                    onLeaveFeedback={countFeedback}
-                />
-            </Section>
-            <Section title={'Statistics'}>
-                {total ? (
-                    <Statistics
-                        good={good}
-                        neutral={neutral}
-                        bad={bad}
-                        total={total}
-                        positivePercentage={percent}
-                    />
-                ) : (
-                    <Notification message="There is no feedback" />
-                )}
-            </Section>
-        </div>
-    );
+  const { good, neutral, bad } = feedback;
+  const total = good + neutral + bad;
+  const percent = Math.floor((good / total) * 100);
+  return (
+    <div style={{ height: '100vh', fontSize: 32, color: '#010101' }}>
+      <Section title={'Please leave feedback'}>
+        <FeedbackOptions
+          options={Object.keys(feedback)}
+          onLeaveFeedback={countFeedback}
+        />
+      </Section>
+      <Section title={'Statistics'}>
+        {total ? (
+          <Statistics
+            good={good}
+            neutral={neutral}
+            bad={bad}
+            total={total}
+            positivePercentage={percent}
+          />
+        ) : (
+          <Notification message="There is no feedback" />
+        )}
+      </Section>
+    </div>
+  );
 };
 
 export default App;
